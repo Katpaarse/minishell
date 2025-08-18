51d2ff83c16146212ec51f040b59dae4847609f1
Julian Kersten <jukerste@f1r6s13.codam.nl>
2025-08-18 19:13:58 +0200

    handling quotes in parsing and tested

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: jukerste <jukerste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 16:19:46 by jukerste          #+#    #+#             */
-/*   Updated: 2025/08/17 15:31:15 by jukerste         ###   ########.fr       */
+/*   Updated: 2025/08/18 16:00:54 by jukerste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ static int	is_special_op(char c)
 // count how many tokens are in the input string, and it treats special operators (>, >>, <, <<, etc.) as separate tokens
 static int	count_tokens(char *input)
 {
-	int	i;
-	int	count;
+	int		i;
+	int		count;
+	char	quote;
 
 	i = 0;
 	count = 0;
-
 	while (input[i])
 	{
 		while (input[i] && is_space(input[i]))
@@ -37,7 +37,17 @@ static int	count_tokens(char *input)
 		if (input[i] == '\0')
 			break;
 		count++;
-		if (is_special_op(input[i]))
+		if (input[i] == '\'' || input[i] == '"')
+		{
+			quote = input[i];
+			i++;
+			while (input[i] && input[i] != quote)
+				i++;
+			if (input[i] == '\0')
+				return (-1); // unclosed quote
+			i++;
+		}
+		else if (is_special_op(input[i]))
 		{
 			if ((input[i] == '>' && input[i + 1] == '>') ||
 				(input[i] == '<' && input[i + 1] == '<'))
@@ -47,8 +57,8 @@ static int	count_tokens(char *input)
 		}
 		else
 		{
-			while (input[i] && !is_space(input[i]) && !is_special_op(input[i]))
-			i++;
+			while (input[i] && !is_space(input[i]) && !is_special_op(input[i]) && input[i] != '\'' && input[i] != '"')
+				i++;
 		}
 	}
 	return (count);
@@ -62,8 +72,11 @@ char	**tokenize_input(char *input)
 	int		start;
 	int		num_tokens;
 	char	**tokens;
+	char	quote;
 	
 	num_tokens = count_tokens(input);
+	if (num_tokens == -1) // unclosed quote
+		return (NULL);
 	tokens = malloc(sizeof(char *) * (num_tokens + 1));
 	if (tokens == NULL)
 		return (NULL);
@@ -75,22 +88,37 @@ char	**tokenize_input(char *input)
 			i++;
 		if (input[i] == '\0')
 			break;
-		start = i;
-		if (is_special_op(input[i]))
+		if (input[i] == '\'' || input[i] == '"')
 		{
+			quote = input[i];
+			i++;
+			start = i;
+			while (input[i] && input[i] != quote)
+				i++;
+			tokens[ti] = ft_strndup(input + start, i - start);
+			ti++;
+			if (input[i] == quote)
+				i++;
+		}
+		else if (is_special_op(input[i]))
+		{
+			start = i;
 			if ((input[i] == '>' && input[i + 1] == '>') ||
 				(input[i] == '<' && input[i + 1] == '<'))
 				i = i + 2;
 			else
 				i++;
+			tokens[ti] = ft_strndup(input + start, i - start);
+			ti++;
 		}
 		else
 		{
-			while (input[i] && !is_space(input[i]) && !is_special_op(input[i]))
+			start = i;
+			while (input[i] && !is_space(input[i]) && !is_special_op(input[i]) && input[i] != '\'' && input[i] != '"')
 				i++;
+			tokens[ti] = ft_strndup(input + start, i - start);
+			ti++;
 		}
-		tokens[ti] = ft_strndup(input + start, i - start);
-		ti++;
 	}
 	tokens[ti] = NULL;
 	return (tokens);
