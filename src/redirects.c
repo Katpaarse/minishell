72e5ab26c5df1d42d4d912c51957a899865f22a2
Lars Van Der Grift <lavan-de@f0r4s24.codam.nl>
2025-09-16 16:25:00 +0200

    is_redirects, finished??

--- src/redirects.c
+++ src/redirects.c
@@ -25,6 +25,7 @@ int	count_redirects(t_redirect *list)
 	}
 	return (len);
 }
+// add a new redirect to the list of redirects
 t_redirect	*add_redirect(t_redirect *list, char *filename, t_redirect_type type)
 {
 	int			i;
@@ -48,4 +49,66 @@ t_redirect	*add_redirect(t_redirect *list, char *filename, t_redirect_type type)
 	new_list[len + 1].type = RED_NONE;
 	free(list);
 	return (new_list);
+}
+
+int	is_redirect(t_cmd *cmd, t_minishell *shell)
+{
+	int fd;
+	int	i;
+
+	if (!cmd || !shell || !cmd->redirects)
+		return (SUCCESS);
+
+	i = 0;
+	while (cmd->redirects && cmd->redirects[i].type != RED_NONE)
+	{
+		if (cmd->redirects[i].type == RED_OUTPUT)
+			fd = open(cmd->redirects[i].filename, O_WRONLY | O_CREAT | O_TRUNC, 0666); 	// '>' TRUNCATE mode
+		else if (cmd->redirects[i].type == RED_APPEND)
+			fd = open(cmd->redirects[i].filename, O_WRONLY | O_CREAT | O_APPEND, 0666); // '>>' APPEND mode
+		else if (cmd->redirects[i].type == RED_INPUT)
+			fd = open(cmd->redirects[i].filename, O_RDONLY);							// '<' INFILE mode
+		else if (cmd->redirects[i].type == RED_HEREDOC)
+		{																				// '<<' HEREDOC 'EOF' mode
+			// implement heredoc handling
+			i++;
+			continue ;
+		}
+		else
+		{
+			i++;
+			continue ;
+		}
+
+		if (fd < 0)
+		{
+			// error handling here
+			return (FAILURE);
+		}
+
+		// Redirect stdout or stdin depending on type
+		if (cmd->redirects[i].type == RED_OUTPUT || cmd->redirects[i].type == RED_APPEND)
+		{
+			if (dup2(fd, 1) < 0)
+			{
+				// error handling here
+				close(fd);
+				return (FAILURE);
+			}
+		}
+		else if (cmd->redirects[i].type == RED_INPUT || cmd->redirects[i].type == RED_HEREDOC)
+		{
+			if (dup2(fd, 0) < 0)
+			{
+				// error handling here
+				close(fd);
+				return (FAILURE);
+			}
+		}
+
+		close(fd);
+		i++;
+	}
+
+	return (SUCCESS);
 }
