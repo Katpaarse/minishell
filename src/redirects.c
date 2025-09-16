12dba94ec70683eb8e83574f05c350f2375047b9
Julian Kersten <jukerste@f0r4s26.codam.nl>
2025-09-16 16:25:31 +0200

    comments on add redirect function

--- src/redirects.c
+++ src/redirects.c
@@ -6,13 +6,13 @@
 /*   By: jukerste <jukerste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 16:04:35 by jukerste          #+#    #+#             */
-/*   Updated: 2025/09/01 16:45:15 by jukerste         ###   ########.fr       */
+/*   Updated: 2025/09/16 15:26:37 by jukerste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// count the amount of redirects found from input
+// count the amount of redirects found from input > < >>
 int	count_redirects(t_redirect *list)
 {
 	int	len;
@@ -32,7 +32,7 @@ t_redirect	*add_redirect(t_redirect *list, char *filename, t_redirect_type type)
 	t_redirect	*new_list;
 
 	len = count_redirects(list);
-	new_list = malloc(sizeof(t_redirect) * (len + 2));
+	new_list = malloc(sizeof(t_redirect) * (len + 2)); // + 1 for new redirect added to list and + 1 for sentinal null terminator for list
 	if (new_list == NULL)
 		return (NULL);
 	i = 0;
